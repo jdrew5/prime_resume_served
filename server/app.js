d7adbe57ea7c10a6b07de2851858f7fac2caa01c
jdrew5 <jdrew5@hotmail.com>
2015-10-29 16:54:24 -0500

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+
+app.set("port", process.env.PORT || 5000);
+
+
+app.get("/*", function(req,res,next){
+    var file = req.params[0] || "/index.html";
+    res.sendFile(path.join(__dirname, "./public/", file))
+});
+
+app.listen(app.get("port"), function(req,res,next){
+    console.log("Listening on port: " + app.get("port"));
+});
+
+module.exports = app;
